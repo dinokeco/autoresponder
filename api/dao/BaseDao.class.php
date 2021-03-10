60bf7decd6bb84daf5447a6ee34ba66046364c38
dinokeco <dino.keco@gmail.com>
2021-03-10 14:29:34 +0100

    add paggination support

--- api/dao/BaseDao.class.php
+++ api/dao/BaseDao.class.php
@@ -78,8 +78,8 @@ class BaseDao {
     return $this->query_unique("SELECT * FROM ".$this->table." WHERE id = :id", ["id" => $id]);
   }
 
-  public function get_all(){
-    return $this->query("SELECT * FROM ".$this->table, []);
+  public function get_all($offset = 0, $limit = 25){
+    return $this->query("SELECT * FROM ".$this->table." LIMIT ${limit} OFFSET {$offset}", []);
   }
 }
 ?>
--- api/test.php
+++ api/test.php
@@ -7,17 +7,9 @@ require_once dirname(__FILE__). "/dao/UserDao.class.php";
 require_once dirname(__FILE__). "/dao/AccountDao.class.php";
 require_once dirname(__FILE__). "/dao/CampaignDao.class.php";
 
-$dao = new CampaignDao();
+$dao = new AccountDao();
 
-$campaing = [
-  "name" => "Flash sale of shooes",
-  "account_id" => 1,
-  "start_date" => date("Y-m-d H:i:s")
-];
-$dao->update(1, [
-  "end_date" => "2021-04-01 00:00:00",
-  "status" => "BLOCKED"
-]);
-$campaing = $dao->get_all_campaings();// $dao->add($campaing);
-print_r($campaing);
+$accounts = $dao->get_all($_GET['offset'], $_GET['limit']);
+print_r($accounts);
+
 ?>
