de886d01a8c1eda143ab9769491fc3aeaf545bbf
dinokeco <dino.keco@gmail.com>
2021-03-10 10:23:00 +0100

    incremental updates

--- api/dao/UserDao.class.php
+++ api/dao/UserDao.class.php
@@ -20,8 +20,14 @@ class UserDao extends BaseDao{
   }
 
   public function update_user($id, $user){
-    $sql = "UPDATE users SET name = :name, email = :email, password = :password, account_id = :account_id WHERE id = :id";
-    $stmt= $this->connection->prepare($sql);
+    $query = "UPDATE users SET ";
+    foreach($user as $name => $value){
+      $query .= $name ."= :". $name. ", ";
+    }
+    $query = substr($query, 0, -2);
+    $query .= " WHERE id = :id";
+
+    $stmt= $this->connection->prepare($query);
     $user['id'] = $id;
     $stmt->execute($user);
   }
--- api/test.php
+++ api/test.php
@@ -10,11 +10,9 @@ $user_dao = new UserDao();
 //$user = $user_dao->get_user_by_id(3);
 
 $user1 = [
-  "name" => "Faris Bektas",
-  "email" => "faris.bektas@stu.ibu.edu.ba",
-  "password" => "12345",
-  "account_id" => 1
+  "password" => "1345"
 ];
+
 $user = $user_dao->update_user(11, $user1);
 
 //$user_dao->get_user_by_email("dino.keco@gmail.com");
