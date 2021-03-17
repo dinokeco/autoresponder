9a5bf128ef6364cb1b2192052d78fa329e160f24
dinokeco <dino.keco@gmail.com>
2021-03-17 09:20:00 +0100

    add parsing of query parameters

--- api/index.php
+++ api/index.php
@@ -6,17 +6,19 @@ error_reporting(E_ALL);
 require_once dirname(__FILE__).'/../vendor/autoload.php';
 require_once dirname(__FILE__).'/dao/AccountDao.class.php';
 
+/* utility function for reading query parameters from URL */
+Flight::map('query', function($name, $default_value = NULL){
+  $request = Flight::request();
+  $query_param = @$request->query->getData()[$name];
+  $query_param = $query_param ? $query_param : $default_value;
+  return $query_param;
+});
+
+/* register DAO layer */
 Flight::register('accountDao', 'AccountDao');
 
+/* include all routes */
 require_once dirname(__FILE__)."/routes/accounts.php";
-
-Flight::route('/', function(){
-    echo 'hello world3!';
-});
-
-Flight::route('/hello5', function(){
-    echo 'hello world5!';
-});
 
 Flight::start();
 ?>
--- api/routes/accounts.php
+++ api/routes/accounts.php
@@ -1,24 +1,27 @@
 <?php
 
 Flight::route('GET /accounts', function(){
-    Flight::json(Flight::accountDao()->get_all(0,10));
+  $offset = Flight::query('offset', 0);
+  $limit = Flight::query('limit', 10);
+  
+  Flight::json(Flight::accountDao()->get_all($offset,$limit));
 });
 
 Flight::route('GET /accounts/@id', function($id){
-    Flight::json(Flight::accountDao()->get_by_id($id));
+  Flight::json(Flight::accountDao()->get_by_id($id));
 });
 
 Flight::route('POST /accounts', function(){
-    $request = Flight::request()->data->getData();
-    Flight::json(Flight::accountDao()->add($data));
+  $request = Flight::request()->data->getData();
+  Flight::json(Flight::accountDao()->add($data));
 });
 
 Flight::route('PUT /accounts/@id', function($id){
-    $request = Flight::request();
-    $data = $request->data->getData();
-    Flight::accountDao()->update($id, $data);
-    $account = Flight::accountDao()->get_by_id($id);
-    Flight::json($account);
+  $request = Flight::request();
+  $data = $request->data->getData();
+  Flight::accountDao()->update($id, $data);
+  $account = Flight::accountDao()->get_by_id($id);
+  Flight::json($account);
 });
 
 ?>
