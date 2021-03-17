561a0c428ed36db04b4d2a181fa0e8bc475fae63
dinokeco <dino.keco@gmail.com>
2021-03-17 14:32:41 +0100

    add get and update of email template

--- api/routes/email_templates.php
+++ api/routes/email_templates.php
@@ -10,10 +10,17 @@ Flight::route('GET /email_templates', function(){
   Flight::json(Flight::emailTemplateService()->get_email_templates($account_id, $offset, $limit, $search, $order));
 });
 
+Flight::route('GET /email_templates/@id', function($id){
+  Flight::json(Flight::emailTemplateService()->get_by_id($id));
+});
+
 Flight::route('POST /email_templates', function(){
   $data = Flight::request()->data->getData();
   Flight::json(Flight::emailTemplateService()->add($data));
 });
 
-
+Flight::route('PUT /email_templates/@id', function($id){
+  $data = Flight::request()->data->getData();
+  Flight::json(Flight::emailTemplateService()->update($id, $data));
+});
 ?>
