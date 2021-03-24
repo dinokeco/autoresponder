868fa7b71838b67ef23e42798c8c89160223fb38
dinokeco <dino.keco@gmail.com>
2021-03-24 11:21:18 +0100

    smtp fix

--- api/clients/SMTPClient.class.php
+++ api/clients/SMTPClient.class.php
@@ -7,10 +7,10 @@ class SMTPClient {
   private $mailer;
 
   public function __construct(){
-    $transport = (new Swift_SmtpTransport(Config::SMTP_HOST, Config::SMTP_PORT))
+    $transport = (new Swift_SmtpTransport(Config::SMTP_HOST, Config::SMTP_PORT, 'tls'))
       ->setUsername(Config::SMTP_USER)
-      ->setPassword(Config::SMTP_PASSWORD)
-    ;
+      ->setPassword(Config::SMTP_PASSWORD);
+
     $this->mailer = new Swift_Mailer($transport);
   }
 
--- api/routes/users.php
+++ api/routes/users.php
@@ -1,5 +1,20 @@
 <?php
 
+/**
+ * @OA\Post(path="/users/register", tags={"users"},
+ *   @OA\RequestBody(description="Basic user info", required=true,
+ *       @OA\MediaType(mediaType="application/json",
+ *    			@OA\Schema(
+ *    				 @OA\Property(property="account", required="true", type="string", example="My Test Account",	description="Name of the account" ),
+ *     				 @OA\Property(property="name", required="true", type="string", example="First Last Name",	description="Name of the user`" ),
+ *    				 @OA\Property(property="email", required="true", type="string", example="myemail@gmail.com",	description="User's email address" ),
+ *             @OA\Property(property="password", required="true", type="string", example="12345",	description="Password" )
+ *          )
+ *       )
+ *     ),
+ *  @OA\Response(response="200", description="Message that user has been created.")
+ * )
+ */
 Flight::route('POST /users/register', function(){
   $data = Flight::request()->data->getData();
   Flight::json(Flight::userService()->register($data));
