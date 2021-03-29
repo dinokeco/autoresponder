e6630d51fdca3e01982ee67784582ee632034a6f
Dino Keco <dino.keco@gmail.com>
2021-03-29 11:37:07 +0200

    other middleware option

--- api/routes/middleware.php
+++ api/routes/middleware.php
@@ -1,9 +1,28 @@
 <?php
-
+/* FILTER BASED MIDDLWARE
 Flight::before('start', function(&$params, &$output){
 
   if (Flight::request()->url == '/swagger') return TRUE;
 
+  if (str_starts_with(Flight::request()->url, '/users/')) return TRUE;
+
+  $headers = getallheaders();
+  $token = @$headers['Authentication'];
+  try {
+    $decoded = (array)\Firebase\JWT\JWT::decode($token, "JWT SECRET", ["HS256"]);
+    Flight::set('user', $decoded);
+    // ADMIN - create set of routes /admin/something
+    // USER - set of routes for regular users
+    // USER_READ_ONLY - block POST and PUT methods
+    return TRUE;
+  } catch (\Exception $e) {
+    Flight::json(["message" => $e->getMessage()], 401);
+    die;
+  }
+}); */
+
+/* ROUTE BASED MIDDLWARE */
+Flight::route('*', function(){
   if (str_starts_with(Flight::request()->url, '/users/')) return TRUE;
 
   $headers = getallheaders();
