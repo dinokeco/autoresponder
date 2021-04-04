d0daf0d4fb12858e0566e2285465ed8868b4b7b7
Dino Keco <dino.keco@gmail.com>
2021-04-04 14:20:05 +0200

    testing env variabales

--- api/config.php
+++ api/config.php
@@ -1,9 +1,12 @@
 <?php
 
+print_r($_ENV);
+die;
+
 class Config {
   const DATE_FORMAT = "Y-m-d H:i:s";
 
-  const DB_HOST = "localhost";
+  const DB_HOST = "autoresponder-db-do-user-1249919-0.b.db.ondigitalocean.com";
   const DB_USERNAME = "autoresponder";
   const DB_PASSWORD = "autoresponder";
   const DB_SCHEME = "autoresponder";
