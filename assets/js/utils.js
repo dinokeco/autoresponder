7cc4de6b5a96e1ac06f6b443a021987df64fa99b
Dino Keco <dino.keco@gmail.com>
2021-05-26 10:30:57 +0200

    small code refactor.

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -1,20 +1,29 @@
-function jsonize_form(selector){
-  var data = $(selector).serializeArray();
-  var form_data = {};
-  for(var i = 0; i < data.length; i++){
-    form_data[data[i].name] = data[i].value;
+class AUtils{
+  
+  static form2json(selector) {
+    var data = $(selector).serializeArray();
+    var form_data = {};
+    for(var i = 0; i < data.length; i++){
+      form_data[data[i].name] = data[i].value;
+    }
+    return form_data;
   }
-  return form_data;
-}
 
-/*
-https://stackoverflow.com/questions/38552003/how-to-decode-jwt-token-in-javascript-without-using-a-library
-*/
-function parse_jwt (token) {
-    var base64Url = token.split('.')[1];
-    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-    var jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
-        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-    }).join(''));
-    return JSON.parse(jsonPayload);
-};
+  static json2form(selector, data){
+    for (attr in data){
+      $(selector+" *[name='"+attr+"']").val(data[attr]);
+    }
+  }
+
+  /*
+  https://stackoverflow.com/questions/38552003/how-to-decode-jwt-token-in-javascript-without-using-a-library
+  */
+  static parse_jwt(token) {
+      var base64Url = token.split('.')[1];
+      var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+      var jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
+          return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+      }).join(''));
+      return JSON.parse(jsonPayload);
+  }
+}
