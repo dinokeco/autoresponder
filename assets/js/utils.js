9fcfae12b9dacd910332c6b6e765a3d9ad0dae26
Dino Keco <dino.keco@gmail.com>
2021-05-26 09:44:39 +0200

    admin stuff

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -1,4 +1,3 @@
-
 function jsonize_form(selector){
   var data = $(selector).serializeArray();
   var form_data = {};
@@ -7,3 +6,15 @@ function jsonize_form(selector){
   }
   return form_data;
 }
+
+/*
+https://stackoverflow.com/questions/38552003/how-to-decode-jwt-token-in-javascript-without-using-a-library
+*/
+function parse_jwt (token) {
+    var base64Url = token.split('.')[1];
+    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    var jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+    return JSON.parse(jsonPayload);
+};
